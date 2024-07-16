cefbf724363210b3cf8796117cfb8b63fdcb474b
Antoine-dSG <adsg96@hku.hk>
2024-07-16 14:16:56 +0800

    add diamond rule definition

--- FriezePatterns/Basic.lean
+++ FriezePatterns/Basic.lean
@@ -7,23 +7,28 @@ open Nat
 -- Functions with a top border of 0s then 1s
 def hasBorderTop (f: ℕ × ℤ → ℤ) :=
   ∀ m, f (0, m) = 0 ∧ f (1, m) = 1
-
+--
+-- Function satisfying the diamond rule
+--
+def hasDiamond (f: ℕ × ℤ → ℤ) :=
+  ∀ i, ∀ m, i ≥ 1 ∧ f (i,m) * f (i,m+1) = 1 + f (i+1,m) * f (i-1, m+1)
+--
 -- Functions with a bottom border of 1s then 0s at the (n+1)th and (n+2)th rows
+--
 def hasBorderBot_n (f: ℕ × ℤ → ℤ) (n : ℕ):=
   ∀ m, f (n+1,m) = 1 ∧ f (n+2,m) = 0
-
+--
 -- Functions with a bottom border of 1s then 0s
+--
 def hasBorderBot (f: ℕ × ℤ → ℤ) :=
   ∃ n, hasBorderBot_n f n
-
--- Functions which are translation-invariant
-def hasTrnsl_n (f: ℕ × ℤ → ℤ) (n : ℕ) :=
+--
+-- Functions which are translation-invariant by n
+--
+def transInva_n (f: ℕ × ℤ → ℤ) (n : ℕ) :=
   ∀ i,∀ m, f (i,m) = f (i,m+n)
-
-
-
-
+--
 variable (f: ℕ × ℤ → ℤ)
 variable (n : ℕ)
 -- Translation-invariance of frieze patterns of width n
-theorem trans_invar (hBT : hasBorderTop f) (hBB : hasBorderBot_n f n) : hasTrnsl_n f (n+3) := by sorry
+theorem hasTransInvar (hBT : hasBorderTop f) (hD : hasDiamond f) (hBB : hasBorderBot_n f n) : transInva_n f (n+3) := by sorry
