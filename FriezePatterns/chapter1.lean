f15309f0149929d6d8db6d0cd8c36d0d9c8c4906
Antoine-dSG <adsg96@hku.hk>
2024-07-22 21:25:16 +0800

    removed Fin n and restated results
    Proof of continuantInftyFrieze very close to being finished. Far too long and tedious. There has to be a better way

--- FriezePatterns/chapter1.lean
+++ FriezePatterns/chapter1.lean
@@ -1,37 +1,85 @@
 import Mathlib.Data.Set.Basic
 import Mathlib.Algebra.Ring.Basic
 import Mathlib.Tactic
-import Mathlib.Data.Finset.Basic
+import Mathlib.Algebra.Group.Defs
 -- open Nat
 -- Second attempt at defining frieze patterns. This time we use the
 -- finite sets Fin and we consider rational valued patterns
 
+-- The above attempt has not worked, because inductions on Fin n are a real problem
+-- Here is a third attempt at a working definition
 
---def border (n : ℕ) (f : Fin (n+2) × ℤ → ℚ) :=
---  ∀ (m : ℤ), f (0,m) =1
+-- Acroynms for the comments:
+-- CfF: Convenient for Formalisation
 
--- def diamond (n : ℕ) (f : Fin (n+2) × ℤ → ℚ) :=
---  ∀ i, ∀ m, 1 ≤ i.1 ∧ i.1 ≤ n ∧ (f (i,m) * f (i,m+1) = 1 + f (i+1,m) * f (i-1, m+1) )
+class pattern (f : ℕ × ℤ → ℚ) : Prop where
+  bordZeros: ∀m, f (0,m) = 0
+  bordOnes : ∀ m, f (1,m) =1
+  diamond : ∀ m, ∀ i,  f (i+1,m) * f (i+1,m+1) =1 + f (i+2,m)*f (i,m+1)
+
+class inftyFrieze (f : ℕ × ℤ → ℚ) extends pattern f where
+  positive : ∀ m, ∀ i, f (i+1,m) > 0
+  non_zero : ∀ m, ∀ i, f (i+1,m) ≠ 0 -- Obviously follows from inftyFrieze.positive, but CfF
+
+lemma continuantInftyFrieze (f : ℕ × ℤ → ℚ) [inftyFrieze f] (i : ℕ) : ∀ m, f (i+2,m) = f (2,m+i) * f (i+1, m) - f (i,m) := by
+  induction i with
+  | zero =>
+  simp
+  intro m
+  have h₀ : f (0,m) = 0 := by exact pattern.bordZeros m
+  have h₁ : f (1,m) = 1 := by exact pattern.bordOnes m
+  rw [h₀, h₁]
+  simp
+  | succ n ih =>
+    intro m
+    have h₂ : f (n + 2, m) = f (2, m + n) * f (n + 1, m) - f (n, m) := by exact ih m
+    have h₃ : f (n+2 ,m) * f (n+2, m+1) = 1 + f (n+3 , m) * f (n+1, m+1) := by exact pattern.diamond m (n + 1)
+    have h₄ : f (n+2, m+1) = f (2, m + 1 + n) * f (n + 1, m+1) - f (n, m+1) := by exact ih (m+1)
+    rw [h₄] at h₃
+    simp [h₃]
+    rw [sub_eq_add_neg] at h₃
+    rw [left_distrib] at h₃
+    have h₅ : 1 = f (n+1, m) * f (n+1, m+1) - f (n+2,m) * f (n,m+1) := by sorry
+    rw [h₅] at h₃
+    rw [mul_neg] at h₃
+    rw [sub_eq_add_neg] at h₃
+    rw [add_comm (f (n + 1, m) * f (n + 1, m + 1) + -(f (n + 2, m) * f (n, m + 1)))] at h₃
+    -- The following hypothesis by sorry is because I gave up with the rewrites
+    have h : f (n + 2, m) * (f (2, m + 1 + n) * f (n + 1, m + 1)) - (f (n + 1, m) * f (n + 1, m + 1)) = f (n + 3, m) * f (n + 1, m + 1) := by sorry
+    rw [sub_eq_add_neg] at h
+    rw [← neg_mul] at h
+    rw [← mul_assoc (f (n + 2, m)),← right_distrib] at h
+    have hh : f (n+1, m+1) ≠ 0 := by exact inftyFrieze.non_zero (m+1) n
+    have hhh : f (n + 2, m) * f (2, m + 1+n ) + -f (n + 1, m) = f (n + 3, m) := by apply mul_right_cancel₀ hh h
+    rw [add_assoc, add_comm 1 2, two_add_one_eq_three, add_assoc n 1, one_add_one_eq_two, sub_eq_add_neg, mul_comm]
+    sorry
+    -- How do we proceed from here ? This is driving me crazy
 
 
--- def positive (n : ℕ) (f : Fin (n+2) × ℤ → ℚ) :=
---  ∀ m, ∀ i, f (i,m) >0
-
---def frieze (n : ℕ) (f : Fin (n+2) × ℤ → ℚ) :=
--- border n f ∧ diamond n f ∧ positive n f
-
-variable (n : ℕ)
-variable (f : Fin (n+2) × ℤ → ℚ)
-
-class Frieze (n : ℕ) (f : Fin (n+2) × ℤ → ℚ) : Prop where
-  diamondrule : ∀ m, ∀ i, f (i+1,m) * f (i+1,m+1) = 1 + f (i+2,m) * f (i, m+1)
-  topBorder1s : ∀ (m : ℤ), f (0,m) =1
-  bottomBorder1s : ∀ (m : ℤ), f (n+1,m) =1
-  positiveValues: ∀ m, ∀ i, f (i,m) >0
-  integral: ∀ m, ∀ i, (f (i,m)).den == 1
 
 
 
+
+
+
+
+class closedPattern  (f : ℕ × ℤ → ℚ) (n : ℕ) : Prop where
+  bordZeros: ∀m, f (0,m) = 0
+  bordOnes : ∀ m, f (1,m) =1
+  diamond : ∀ m, ∀ i,  f (i+1,m) * f (i+1,m+1) =1 + f (i+2,m)*f (i,m+1)
+  width_n: ∀m, f (n+2,m) = 1
+-- Careful that the n here is NOT the width; it is the width - 2. I believe this definition is more convenient for formalisation
+
+def set_1_to_n (n : Nat) : Set Nat :=
+  { i | 1 ≤ i ∧ i ≤ n }
+
+-- The class below looks a bit dubious to me
+class closedFrieze (f : ℕ × ℤ → ℚ) (n : ℕ) extends closedPattern f n where
+  positive_n : ∀m, ∀ i ∈ (set_1_to_n n), f (i,m) > 0
+
+
+
+/--  OLD CODE
 
 example (f : Fin (n+2) × ℤ → ℚ) [Frieze n f] (m : ℤ): f (0,m) =1 := by
   apply Frieze.topBorder1s
@@ -69,5 +117,6 @@ lemma base_continuant (m : ℤ)  (f : Fin (n+2) × ℤ → ℚ) [Frieze n f] : f
 
 
 
-lemma continuant (n : ℕ) (f : Fin (n+2) × ℤ → ℚ) [Frieze n f] : ∀ m, ∀ i, f (i+2,m) = f (i+1,m)* f (1,m+i+1) - f (i,m) := by  sorry
--- How do we make a proof by induction?
+lemma continuant (n : ℕ) (f : Fin (n+2) × ℤ → ℚ) [Frieze n f] : ∀ i, ∀ m, f (i+2,m) = f (i+1,m)* f (1,m+i+1) - f (i,m) := by sorry
+
+-/
