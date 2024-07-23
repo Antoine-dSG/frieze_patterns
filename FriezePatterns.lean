903ee8a3d893a1f690e36f9ffd886e0be6ee0d12
Antoine-dSG <adsg96@hku.hk>
2024-07-23 11:50:39 +0800

    fix FriezePatterns.lean

--- FriezePatterns.lean
+++ FriezePatterns.lean
@@ -1,4 +1,2 @@
--- This module serves as the root of the `FriezePatterns` library.
--- Import modules here that should be built as part of the library.
-import «FriezePatterns».chapter1
-import «FriezePatterns».chapter2
+import FriezePatterns.chapter1
+import FriezePatterns.chapter2
