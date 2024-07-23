3b067c136e1346b2db7672d6f4680f378b74723f
Antoine-dSG <adsg96@hku.hk>
2024-07-23 17:20:43 +0800

    new definitions

--- FriezePatterns/chapter1.lean
+++ FriezePatterns/chapter1.lean
@@ -15,47 +15,39 @@ import Mathlib.Algebra.Group.Defs
 class pattern (f : ℕ × ℕ → ℚ) : Prop where
   bordZeros: ∀m, f (0,m) = 0
   bordOnes : ∀ m, f (1,m) =1
-  diamond : ∀ m, ∀ i,  f (i+1,m) * f (i+1,m+1) =1 + f (i+2,m)*f (i,m+1)
+  diamond : ∀ m, ∀ i,  f (i+1,m) * f (i+1,m+1) -1= f (i+2,m)*f (i,m+1)
 
 class inftyFrieze (f : ℕ × ℕ → ℚ) extends pattern f where
   positive : ∀ m, ∀ i, f (i+1,m) > 0
   non_zero : ∀ m, ∀ i, f (i+1,m) ≠ 0 -- Obviously follows from inftyFrieze.positive, but CfF
 
-lemma continuantInftyFrieze (f : ℕ × ℕ → ℚ) [inftyFrieze f] (i : ℕ) : ∀ m, f (i+2,m) = f (2,m+i) * f (i+1, m) - f (i,m) := by
+lemma scaledContinuant (f : ℕ×ℕ → ℚ) [inftyFrieze f] (i : ℕ) : ∀m, f (i+2,m) * f (i,m+1) = (f (2,m+i)*f (i+1,m) - f (i,m))* f (i,m+1) := by
   induction i with
   | zero =>
-  simp
   intro m
   have h₀ : f (0,m) = 0 := by exact pattern.bordZeros m
   have h₁ : f (1,m) = 1 := by exact pattern.bordOnes m
-  rw [h₀, h₁]
+  rw [h₀,h₁]
   simp
   | succ n ih =>
-    intro m
-    have h₂ : f (n + 2, m) = f (2, m + n) * f (n + 1, m) - f (n, m) := by exact ih m
-    have h₃ : f (n+2 ,m) * f (n+2, m+1) = 1 + f (n+3 , m) * f (n+1, m+1) := by exact pattern.diamond m (n + 1)
-    have h₄ : f (n+2, m+1) = f (2, m + 1 + n) * f (n + 1, m+1) - f (n, m+1) := by exact ih (m+1)
-    rw [h₄] at h₃
-    rw [sub_eq_add_neg] at h₃
-    rw [left_distrib] at h₃
-    have h₅ : 1 = f (n+1, m) * f (n+1, m+1) - f (n+2,m) * f (n,m+1) := by
-      calc 1 = 1 + (f (n + 2, m) * f (n, m + 1)) - (f (n + 2, m) * f (n, m + 1)) := by exact Eq.symm (add_sub_cancel_right 1 (f (n + 2, m) * f (n, m + 1)))
-      _= (1 + f (n + 2, m) * f (n, m + 1)) - (f (n + 2, m) * f (n, m + 1)) := by exact rfl
-      _= f (n+1, m) * f (n+1, m+1) - (f (n + 2, m) * f (n, m + 1)) := by rw [pattern.diamond]
-    rw [mul_neg] at h₃
-    rw [← sub_eq_add_neg] at h₃
-    -- The following hypothesis by sorry is because I gave up with the rewrites
-    have h : f (n + 2, m) * (f (2, m + (n + 1)) * f (n + 1, m + 1)) - (f (n + 1, m) * f (n + 1, m + 1)) = f (n + 3, m) * f (n + 1, m + 1) := by sorry
-    rw [h₅] at h₃
-    rw [add_comm (f (n + 1, m) * f (n + 1, m + 1) - f (n + 2, m) * f (n, m + 1))] at h₃
+  intro m
+  have h₂ : f (n+2,m+1) * f (n,m+1+1) = (f (2,m+1+n)*f (n+1,m+1) - f (n,m+1)) * f (n,m+2) := by exact ih (m + 1)
+  have h₃ : f (n,m+2) ≠ 0 := by sorry -- This "should" just be proved by exact inftyFrieze.non_zero, but there is a problem with f (n,m) = f (n-1+1,m)..
+  calc f (n + 1 + 2, m) * f (n + 1, m + 1) = f (n+3,m)* f (n+1, m+1) := by simp
+  _= f (n+2, m) * f (n+2,m+1) - 1 := by exact Eq.symm (pattern.diamond m (n + 1))
+  _= f (n+2, m) * (f (n+2,m+1) * f (n,m+1+1) * (f (n,m+2))⁻¹) -1 := by sorry -- we need to prove that a = f (n,m+1+1) has an inverse
+  _= f (n+2, m) * ((f (n+2,m+1) * f (n,m+1+1)) * (f (n,m+2))⁻¹) -1 := by rw [mul_assoc]
+  _= f (n+2, m) * ((f (2,m+1+n)*f (n+1,m+1) - f (n,m+1)) * f (n,m+2) * (f (n,m+2))⁻¹) -1 := by rw [h₂]
+  _= f (n+2, m) * (f (2,m+1+n)*f (n+1,m+1) - f (n,m+1)) * f (n,m+2) * (f (n,m+2))⁻¹ -1 := by ring
+  _= f (n+2, m) * ((f (2,m+1+n)*f (n+1,m+1) - f (n,m+1))) -1 := by sorry -- we need to prove that a * a⁻¹ = 1
+  _= f (n+2, m) * f (2,m+1+n)*f (n+1,m+1) - f (n+2, m) *f (n,m+1) -1 := by ring
+  _= f (n+2, m) * f (2,m+1+n)*f (n+1,m+1) - ( f (n+1, m)*f (n+1,m+1) -1 ) -1 := by rw [Eq.symm (pattern.diamond m n)]
+  _= (f (n+2, m) * f (2,m+1+n) - f (n+1, m))*f (n+1,m+1) := by ring
+  _= (f (2,m+1+n)*f (n+2, m) - f (n+1, m))*f (n+1,m+1) := by rw [mul_comm (f (n+2, m))]
+  _= (f (2,m+1+n)*f (n+1+1, m) - f (n+1, m))*f (n+1,m+1) := by ring
+  _= (f (2, m + (n+1)) * f (n + 1 + 1, m) - f (n + 1, m)) * f (n + 1, m + 1) := by rw [add_assoc m 1 n, add_comm 1 n]
 
-    rw [sub_eq_add_neg] at h
-    rw [← neg_mul] at h
-    rw [← mul_assoc (f (n + 2, m)),← right_distrib] at h
-    have hh : f (n+1, m+1) ≠ 0 := by exact inftyFrieze.non_zero (m+1) n
-    have hhh : f (n + 2, m) * f (2, m + (n+1) ) + -f (n + 1, m) = f (n + 3, m) := by apply mul_right_cancel₀ hh h
-    rw [add_assoc, add_comm 1 2, two_add_one_eq_three, add_assoc n 1, one_add_one_eq_two, sub_eq_add_neg, mul_comm]
-    exact id (Eq.symm hhh)
+
 
 
 class closedPattern  (f : ℕ × ℕ → ℚ) (n : ℕ) : Prop where
