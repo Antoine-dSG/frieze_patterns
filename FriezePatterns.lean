286169ff319bea9043f346936db8001ca96345bb
Antoine-dSG <adsg96@hku.hk>
2024-07-26 18:28:34 +0800

    fixed imports

--- FriezePatterns.lean
+++ FriezePatterns.lean
@@ -1,2 +1,3 @@
-import FriezePatterns.chapter1
-import FriezePatterns.chapter2
+import FriezePatterns.chapter1.section1
+import FriezePatterns.chapter1.section2
+import FriezePatterns.chapter1.section3
