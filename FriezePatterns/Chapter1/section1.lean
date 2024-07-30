4fc1e0100e45ad9b8dcdc4f4e59ebf63aab501d3
Antoine-dSG <adsg96@hku.hk>
2024-07-30 16:09:10 +0800

    finished translation proof, up to N-arithmetic

--- FriezePatterns/Chapter1/section1.lean
+++ FriezePatterns/Chapter1/section1.lean
@@ -46,17 +46,20 @@ lemma inftyContinuant (F : Type*) [Field F] (f : ℕ×ℕ → F) [nzPattern F f]
 class closedPattern (F : Type*) [Field F] (f : ℕ × ℕ → F) extends pattern F f where
   bounded: ∃ (n : ℕ), ∀m, f (n+1,m) = 1
 
+
 class pattern_n (F : Type*) [Field F] (f : ℕ × ℕ → F) (n : ℕ) : Prop where
   botBordOnes_n : ∀ m, f (n+1, m) = 1
   topBordOnes : ∀ m, f (0,m) =1
   diamond : ∀ m, ∀ i, f (i+1,m) * f (i+1,m+1)-1 = f (i+2,m)*f (i,m+1)
 
 class nzPattern_n (F : Type*) [Field F] (f : ℕ × ℕ → F) (n : ℕ) extends pattern_n F f n where
-  non_zero : ∀ i, ∀ m, i ≤ n → f (i,m) ≠ 0
+  non_zero : ∀ i, ∀ m, i ≤ n+1 → f (i,m) ≠ 0
   botBordZeroes : ∀ m, f (n+2,m) = 0
 
 -- Continuant property for patterns of finite width
-lemma finiteContinuant (F : Type*) [Field F] (f : ℕ×ℕ → F) (n i: ℕ) [nzPattern_n F f n] (hi: i ≤ n) : ∀m, f (i+2,m) = f (1,m+i+1)*f (i+1,m) - f (i,m) := by
+-- This is probably a better formulation:
+lemma finiteContinuant (F : Type*) [Field F] (f : ℕ×ℕ → F) (n: ℕ) [nzPattern_n F f n] : ∀ i, i ≤ n+1 → ∀m, f (i+2,m) = f (1,m+i+1)*f (i+1,m) - f (i,m) := by sorry
+lemma finiteContinuantOld (F : Type*) [Field F] (f : ℕ×ℕ → F) (n i: ℕ) [nzPattern_n F f n] (hi: i ≤ n+1) : ∀m, f (i+2,m) = f (1,m+i+1)*f (i+1,m) - f (i,m) := by
  induction i with
  | zero =>
   intro m
@@ -82,35 +85,57 @@ lemma finiteContinuant (F : Type*) [Field F] (f : ℕ×ℕ → F) (n i: ℕ) [nz
   _= f (1, m +1 + k + 1)*f (k+2,m) - f (k+1,m) := by rw [mul_inv_cancel_right₀ h₂ (f (1, m +1 + k + 1)*f (k+2,m)), mul_inv_cancel_right₀ h₂ (f (k+1,m))]
   _= f (1, m + (k + 1) + 1) * f (k + 1 + 1, m) - f (k + 1, m) := by ring
 
+-- This is a finiteContinuant lemma "in the other direction"
+lemma reverseFiniteContinuant (F : Type*) [Field F] (f : ℕ×ℕ → F) (n: ℕ) [nzPattern_n F f n] : ∀ i, i ≤ n+1 → ∀m, f (i,m+2) = f (n,m+2)*f (i+1,m+1) - f (i+2,m) := by sorry
 
--- This is the first step in proving the glide symmetry
-lemma firstRowSymm (F : Type*) [Field F] (f : ℕ×ℕ → F) (n : ℕ) [nzPattern_n F f n] : ∀ m, f (1,m+n+1) = f (n,m) := by
+lemma glide0 (F : Type*) [Field F] (f : ℕ×ℕ → F) (n: ℕ) [nzPattern_n F f n] : ∀m, f (0,m) = f (n+1, m+1) := by
   intro m
-  have h₁ : f (n+1,m) = 1 := by exact pattern_n.botBordOnes_n m
-  have h₂ : f (n+2,m) = 0 := by exact nzPattern_n.botBordZeroes m
-  have h₃ : n ≤ n := by rfl
-  calc f (1,m+n+1) =  f (1,m+n+1)*1 - 0 := by simp
-  _= f (1,m+n+1)*1 - f (n+2,m) := by rw [h₂]
-  _= f (1,m+n+1) * f (n+1,m) - f (n+2,m) := by rw [h₁]
-  _= f (1,m+n+1) * f (n+1,m) - (f (1,m+n+1)*f (n+1,m) - f (n,m)) := by rw [finiteContinuant F f n n h₃ m]
-  _= f (n,m) := by simp
+  have h₁ : f (0,m) = 1 := by exact pattern_n.topBordOnes n m
+  have h₂ : f (n+1,m+1) = 1 := by exact pattern_n.botBordOnes_n (m+1)
+  rw [h₁,h₂]
 
--- Perhaps we could write a continuant relation "going up", and use this for the induction...
+lemma glide1 (F : Type*) [Field F] (f : ℕ×ℕ → F) (n: ℕ) [nzPattern_n F f n] : ∀m, f (1,m) = f (n, m+2) := by sorry
+  -- This is easy, to be done soon
 
-theorem glideSymm (F : Type*) [Field F] (f : ℕ×ℕ → F) (n: ℕ) [nzPattern_n F f n]  : ∀i, ∀m, i ≤ n+1 → f (n+1-i,m) = f (i,m+n+2-i) := by
- apply Nat.twoStepInduction
- intro m
- simp
- have h₁ : f (0,m+n+2) = 1 ∧ f (n+1,m) = 1 := by exact ⟨pattern_n.topBordOnes n (m+n+2), pattern_n.botBordOnes_n m⟩
- rw [h₁.1,h₁.2]
- intro m
- simp
- have h : f (1,m+n+1) = f (n,m) := by exact firstRowSymm F f n m
- exact Eq.symm h
- sorry
+--
+lemma glideSymm (F : Type*) [Field F] (f : ℕ×ℕ → F) (n: ℕ) [nzPattern_n F f n] : ∀ i, (i ≤ n+1) → ∀m, f (i,m) = f (n+1-i, m+i+1) := by
+  apply Nat.twoStepInduction
+  ·intro h
+   simp
+   exact glide0 F f n
+  ·intro h m
+   simp
+   rw [add_assoc]
+   simp
+   exact glide1 F f n m
+  ·intro i Pi Pii
+   simp at Pi
+   simp at Pii
+   simp
+   intro h
+   have h' : i ≤ n+1 := by linarith
+   have h'' : n - (i+1) ≤ n+1 := by
+    have h₄ : n - (i+1) ≤ n := by simp
+    linarith
+   have hi : ∀ (m : ℕ), f (i, m) = f (n + 1 - i, m + i + 1) := by
+    apply Pi
+    linarith
+   have hii : ∀ (m : ℕ), f (i + 1, m) = f (n - i, m + (i + 1) + 1) := by
+    apply Pii
+    linarith
+   intro m
+   calc f (i + 1 + 1, m) = f (i+2,m) := by simp
+   _= f (1,m+i+1)*f (i+1,m) - f (i,m) := by exact finiteContinuant F f n i h' m
+   _= f (1,m+i+1)*f (i+1,m) - f (n + 1 - i, m + i + 1) := by rw [hi m]
+   _= f (1,m+i+1)* f (n - i, m + (i + 1) + 1) - f (n + 1 - i, m + i + 1) := by rw [hii m]
+   _= f (n, m+i+1+2)* f (n - i, m + (i + 1) + 1) - f (n + 1 - i, m + i + 1) := by rw [glide1 F f n (m+i+1)]
+   _= f (n,(m+i+1)+2)* f (n - i, (m + i + 1) + 1)-f (n + 1 - i, m + i + 1) := by rw [add_assoc m]
+   _= f (n,(m+i+1)+2)*f (n - (i+1)+1, (m + i + 1) + 1) - f (n -(i+1)+2, m + i + 1) := by sorry -- This is arithmetic in ℕ nonsense
+   _= f (n - (i+1), (m+i+1)+2) := by exact Eq.symm (reverseFiniteContinuant F f n (n - (i+1)) h'' (m+i+1))
 
 
--- This is a consequence of the glide symmetry
-lemma translateInvar (F : Type*) [Field F] (f : ℕ×ℕ → F) (i n: ℕ) [nzPattern_n F f n] (hi : i ≤ n+1) : ∀ m, f (i,m) = f (i,m+n+3) := by
-  intro m
-  sorry
+lemma translateInvar (F : Type*) [Field F] (f : ℕ×ℕ → F) (n: ℕ) [nzPattern_n F f n] : ∀ i, i ≤ n+1 → ∀ m, f (i,m) = f (i,m+n+3) := by
+  intro i h m
+  calc f (i,m) = f (n+1-i, m+i+1) := by sorry -- glide symm
+  _= f (n+1-(n+1-i), (m+i+1)+(n+1-i)+1) := by sorry -- glide symm again
+  _= f (i,m+n+3) := by sorry -- arithmetic in ℕ
