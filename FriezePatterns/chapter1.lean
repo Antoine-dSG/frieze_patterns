2402eb3cd1ac177b72b1795aade0900773fc3181
JonCheah <jon.mh.cheah@gmail.com>
2024-08-15 13:47:54 +0800

    Update chapter1.lean

--- FriezePatterns/chapter1.lean
+++ FriezePatterns/chapter1.lean
@@ -1,6 +1,9 @@
 import Mathlib.Algebra.Ring.Basic
 import Mathlib.Tactic
 
+
+-- NEED TO CHANGE DEFINITIONS FROM WIDTH TO HEIGHT: WIDTH n <=> HEIGHT n+2
+----------- SECTION 1 ---------
 ---- Field-valued patterns ----
 class pattern_n (F : Type*) [Field F] (f : ℕ × ℕ → F) (n : ℕ) : Prop where
   topBordZeros : ∀ m, f (0,m) = 0
@@ -42,56 +45,15 @@ lemma pattern_nContinuant1 (F : Type*) [Field F] (f : ℕ×ℕ → F) (n: ℕ) [
 
 lemma pattern_nContinuant2 (F : Type*) [Field F] (f : ℕ×ℕ → F) (n: ℕ) [nzPattern_n F f n] : ∀ i, i ≤ n-1 → ∀m, f (i,m) = f (n-1,m)*f (i+1,m-1) - f (i+2,m-2) := by sorry
 
-theorem trsltInv (F : Type*) [Field F] (f : ℕ×ℕ → F) (n: ℕ) [nzPattern_n F f n] : ∀ i, i ≤ n+1 → ∀m, f (i,m) = f (i,m+n+1) := by
-  suffices glideSymm : ∀ i, i ≤ n+1 → ∀ m, f (n+1-i, m+i) = f (i,m)
-  -- it suffices to prove glide symmetry
-  · intros i ileq m
-    have key := glideSymm i ileq m
-    have key2 := glideSymm (n+1-i) (Nat.sub_le (n+1) i) (m+i)
-    simp [Nat.sub_sub_eq_min, ileq, add_assoc] at key2
-    rw [←key, ←key2, add_assoc]
+theorem trsltInv (F : Type*) [Field F] (f : ℕ×ℕ → F) (n: ℕ) [nzPattern_n F f n] : ∀ i, i ≤ n+1 → ∀m, f (i,m) = f (i,m+n+1) := by sorry
 
-  -- proof of glide symmetry
-  · intros i ileq m
-    induction' i using Nat.strong_induction_on with i ih -- strong induction on i
-    match i with
-    | 0 =>
-      simp
-      rw [@pattern_n.botBordZeros_n F _ f n _ (n+1) m (by linarith), @pattern_n.topBordZeros F _ f n _ m]
-    | 1 =>
-      simp at *
-      rw [@pattern_n.botBordOnes_n F _ f n _ (m+1), @pattern_n.topBordOnes F _ f n _ m]
-    | i+2 =>
-      simp at *
-      rw [Nat.sub_add_eq, ← add_assoc m i 2]
-      have h₁ : i ≤ n-1 := by
-        match n with
-        | 0 => linarith
-        | n+1 => linarith [Nat.add_sub_cancel n 1]
-      have h₂ : f (2,m+i) = f (n-1,m+i+2) := by sorry -- P₂
-      have h₃ : f (i+1,m) = f (n-i,m+i+1) := by
-        have := ih (i+1) (by linarith) (by linarith)
-        simp at this
-        rw [← this, add_assoc]
-      have h₄ : f (i,m) = f (n+1-i,m+i) := by
-        have := ih i (by linarith) (by linarith)
-        simp at this
-        rw [← this]
-      have h₅ : f (n-i-1,m+i+2) = f (n-1,m+i+2) * f (n-i,m+i+1) - f (n+1-i,m+i) := by
-        have key := pattern_nContinuant2 F f n (n-i-1) (by rw [Nat.sub_sub, add_comm i 1, ← Nat.sub_sub]; exact Nat.sub_le (n-1) i) (m+i+2)
-        rw [key]
-        rw [Nat.sub_sub n i 1, ← Nat.sub_add_comm ileq, ← Nat.sub_add_comm ileq]
-        simp
-      symm
-      calc
-        f (i+2,m) = f (2,m+i) * f (i+1,m) - f (i,m) := by rw [pattern_nContinuant1 F f n i h₁ m]
-                _ = f (n-1,m+i+2) * f (n-i,m+i+1) - f (n+1-i,m+i) := by rw [h₂, h₃, h₄]
-                _ = f (n-i-1,m+i+2) := by rw [h₅]
+-- This is surely in mathlib, in some form
+def isFiniteSet (g : ℕ×ℕ → F) : Prop :=
+  ∃ (s : Finset F), ∀ i, ∀m, g (i,m) ∈ s
 
-lemma imageFinite (F : Type*) [Field F] (f : ℕ×ℕ → F) (n: ℕ) [nzPattern_n F f n] : (Set.range f).Finite := by sorry
+lemma imageFinite (F : Type*) [Field F] (f : ℕ×ℕ → F) (n: ℕ) [nzPattern_n F f n] : isFiniteSet f := by sorry
 
-lemma testEqualPattern (F : Type*) [Field F] (f g : ℕ×ℕ → F) (n: ℕ) (hf : nzPattern_n F f n) (hg : nzPattern_n F g n) (h : ∀ i, i ≤ n → f (i,0) = g (i,0)) : f = g := sorry
-/- Antoine: I have put the proof in comments for the moment to avoids bugs during compilation on GitHub pages
+lemma testEqualPattern (F : Type*) [Field F] (f g : ℕ×ℕ → F) (n: ℕ) (hf : nzPattern_n F f n) (hg : nzPattern_n F g n) (h : ∀ i, i ≤ n → f (i,0) = g (i,0)) : f = g := by
   funext ⟨i, m⟩
 
   induction m with
@@ -104,38 +66,80 @@ lemma testEqualPattern (F : Type*) [Field F] (f g : ℕ×ℕ → F) (n: ℕ) (hf
     rw[this,that]
 
   | succ k ih =>
-  -- assume f (i, k) = g (i, k) for all i, want to prove f (i, k + 1) = g (i, k + 1) for all i
+  -- assume f (i, k) = g (i, k) for i, want to prove f (i, k + 1) = g (i, k + 1) for all i
 
   induction i with
+  --induction' i using Nat.twoStepInduction with i' IH1 IH2
+  --. calc f (0,k+1) = 0 := by rw [hf.topBordZeros (k+1)]
+  --    _ = g (0,k+1) := by rw [hg.topBordZeros (k+1)]
+
+  --. calc f (1,k+1) = 1 := by rw [hf.topBordOnes (k+1)]
+  --    _ = g (1,k+1) := by rw [hg.topBordOnes (k+1)]
+  --.
+
     | zero =>
     have this := hf.topBordZeros (k+1)
     have that := hg.topBordZeros (k+1)
     rw[this,that]
-    --proved f (0, k + 1) = g (0, k + 1)
+    ----proved f (0, k + 1) = g (0, k + 1)
 
     | succ i' ih' =>
-    --assume f (i', k + 1) = g (i', k + 1), want to prove f (i', k + 1) = g (i', k + 1)
-
-
-
+    --assume that f (i', k) = g (i', k) implies f (i', k+1) = g (i', k+1).
+    --want to prove f (i', k + 1) = g (i', k + 1)
 
     by_cases i_plus_one_leq_n : i' + 1 ≤ n
-    by_cases one_leq_i_plus_1 : 1 ≤ i' + 1
-
-
-    have i_leq_n_sub_one : i' ≤ n - 1 := by sorry
-    have this : f (i', k+1) =  g (i', k+1) := by exact ih' ih
-    have that : f (i' + 2, k) = g (i' + 2, k) := by sorry
+    have one_leq_i_plus_1 : 1 ≤ i' + 1 :=
+      calc 1 ≤ 0 + 1 := by simp
+            _≤ i'+ 1 := Nat.succ_le_succ (Nat.zero_le i')
 
     have nzf : f (i' + 1, k) ≠ 0 := by apply hf.non_zero (i'+1) k one_leq_i_plus_1 i_plus_one_leq_n
     have nzg : g (i' + 1, k) ≠ 0 := by apply hg.non_zero (i'+1) k one_leq_i_plus_1 i_plus_one_leq_n
 
-    calc f (i' + 1, k + 1) = f (i' + 1, k + 1) * f (i' + 1, k) * (f (i' + 1, k))⁻¹ := by rw[mul_inv_cancel_right₀ nz (f (i' + 1, k + 1))]
+
+    have i_leq_n_sub_one : i' ≤ n - 1 :=
+      calc i'≤ (i'+1).pred := by simp
+            _≤ (n).pred :=  Nat.pred_le_pred i_plus_one_leq_n
+            _≤ n - 1 := by simp
+
+    have eq_top : f (i', k+1) = g (i', k+1) := by sorry
+
+    have eq_bot : f (i' + 2, k) = g (i' + 2, k) := by sorry
+
+    have eq_left : f (i' + 1, k) = g (i' + 1, k) := by exact ih
+
+    calc f (i' + 1, k + 1) = f (i' + 1, k + 1) * f (i' + 1, k) * (f (i' + 1, k))⁻¹ := by rw[mul_inv_cancel_right₀ nzf (f (i' + 1, k + 1))]
     _= ( f (i' + 1, k) * f (i' + 1, k + 1) - 1 + 1) * (f (i' + 1, k))⁻¹ := by ring
     _= ( f (i' + 2,k)*f (i',k+1) + 1) * (f (i' + 1, k))⁻¹ := by rw[hf.diamond i' k i_leq_n_sub_one]
-    _= ( f (i' + 2, k)*f (i',k+1) + 1) * (g (i' + 1, k))⁻¹ := by rw[ih]
-    _= ( f (i' + 2, k)*g (i',k+1) + 1) * (g (i' + 1, k))⁻¹ := by rw[this]
-    _= ( g (i' + 2, k)*g (i',k+1) + 1) * (g (i' + 1, k))⁻¹ := by rw[that]
+    _= ( g (i' + 2, k)*g (i',k+1) + 1) * (g (i' + 1, k))⁻¹ := by rw[eq_left, eq_top, eq_bot]
+    _= ( g (i' + 1, k) * g (i' + 1, k + 1) - 1 + 1) * (g (i' + 1, k))⁻¹ := by rw[hg.diamond i' k i_leq_n_sub_one]
+    _= g (i' + 1, k + 1) *  g (i' + 1, k) * (g (i' + 1, k))⁻¹ := by ring
+    _= g (i' + 1, k + 1) := by rw[mul_inv_cancel_right₀ nzg (g (i' + 1, k + 1))]
+
+
+
+lemma testEqualPattern1 (F : Type*) [Field F] (f g : ℕ×ℕ → F) (n: ℕ) (hf : nzPattern_n F f n) (hg : nzPattern_n F g n) (h : ∀ i, i ≤ n → f (i,0) = g (i,0)) : f = g := by
+  have diamond_right (i' k : ℕ)(eq_top : f (i', k+1) = g (i', k+1))(eq_bot : f (i' + 2, k) = g (i' + 2, k))( eq_left : f (i' + 1, k) = g (i' + 1, k))(nzf : f (i' + 1, k) ≠ 0) (nzg : g (i' + 1, k) ≠ 0 ) : f (i' + 1, k + 1)= g (i' + 1, k + 1) := by
+    have one_leq_i_plus_1 : 1 ≤ i' + 1 :=
+      calc 1 ≤ 0 + 1 := by simp
+            _≤ i'+ 1 := Nat.succ_le_succ (Nat.zero_le i')
+
+    by_cases i_plus_one_geq_n_plus_one : i' + 1 ≥ n+1
+
+    have this := hf.botBordZeros_n (i'+1) (k+1) (by linarith)
+    have that := hg.botBordZeros_n (i'+1) (k+1) (by linarith)
+    rw[this,that]
+
+    have i_plus_one_leq_n : i'+ 1 ≤ n := by linarith
+
+    have i_leq_n_sub_one : i' ≤ n - 1 :=
+      calc i'≤ (i'+1).pred := by simp
+            _≤ (n).pred :=  Nat.pred_le_pred i_plus_one_leq_n
+            _≤ n - 1 := by simp
+
+    calc f (i' + 1, k + 1) = f (i' + 1, k + 1) * f (i' + 1, k) * (f (i' + 1, k))⁻¹ := by rw[mul_inv_cancel_right₀ nzf (f (i' + 1, k + 1))]
+    _= ( f (i' + 1, k) * f (i' + 1, k + 1) - 1 + 1) * (f (i' + 1, k))⁻¹ := by ring
+    _= ( f (i' + 2,k)*f (i',k+1) + 1) * (f (i' + 1, k))⁻¹ := by rw[hf.diamond i' k i_leq_n_sub_one]
+    _= ( g (i' + 2, k)*g (i',k+1) + 1) * (g (i' + 1, k))⁻¹ := by rw[eq_left, eq_top, eq_bot]
     _= ( g (i' + 1, k) * g (i' + 1, k + 1) - 1 + 1) * (g (i' + 1, k))⁻¹ := by rw[hg.diamond i' k i_leq_n_sub_one]
     _= g (i' + 1, k + 1) *  g (i' + 1, k) * (g (i' + 1, k))⁻¹ := by ring
     _= g (i' + 1, k + 1) := by rw[mul_inv_cancel_right₀ nzg (g (i' + 1, k + 1))]
@@ -144,11 +148,33 @@ lemma testEqualPattern (F : Type*) [Field F] (f g : ℕ×ℕ → F) (n: ℕ) (hf
 
 
 
+  funext ⟨i, m⟩
 
 
 
 
 
+------------- SECTION 2 ------------
+---- Arithmetic frieze patterns ----
+class positivePattern_n (f : ℕ × ℕ → ℚ) (n : ℕ) extends nzPattern_n ℚ f n where
+  positive: ∀ i, ∀ m, 1 ≤ i → i ≤ n → f (i,m) >0
+
+def PosPat(n) : Set (ℕ × ℕ → ℚ) := {f : ℕ × ℕ → ℚ | ∃ e : positivePattern_n f}
+-- Need to add a definition of PosPat(n), the set of positive patterns
 
 
--/
+
+lemma positivePatternCharact : 2-1=1 := by sorry
+
+class arith_fp (f : ℕ × ℕ → ℚ) (n : ℕ) extends positivePattern_n f n where
+  integral: ∀ i, ∀ m, (f (i,m)).den == 1
+
+-- Need to add definition of Frieze(n)
+
+class nDiag : Prop where
+
+lemma bijFriezeToDiag : 1+1 = 2 := by sorry
+
+lemma friezeNonEmpty : 1-1 = 0 := by sorry
+
+theorem friezeFinite : 2^2 = 4 := by sorry
