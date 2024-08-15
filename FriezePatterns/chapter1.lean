a43ac5acdb1985b342d4e6dbda310e793308e10f
JonCheah <jon.mh.cheah@gmail.com>
2024-08-15 19:51:08 +0800

    Update chapter1.lean

--- FriezePatterns/chapter1.lean
+++ FriezePatterns/chapter1.lean
@@ -59,45 +59,63 @@ lemma pattern_nContinuant1 (F : Type*) [Field F] (f : ℕ×ℕ → F) (n: ℕ) [
   exact h₂
 
 -- The second continuant lemma is proved like the first
-lemma pattern_nContinuant2 (F : Type*) [Field F] (f : ℕ×ℕ → F) (n: ℕ) [nzPattern_n F f n] : ∀ i, i ≤ n-1 → ∀m, f (i,m) = f (n-1,m)*f (i+1,m-1) - f (i+2,m-2) := by sorry
+lemma pattern_nContinuant2 (F : Type*) [Field F] (f : ℕ×ℕ → F) (n: ℕ) [nzPattern_n F f n] : ∀ i, i ≤ n-1 → ∀m, f (i,m+2) = f (n-1,m+2)*f (i+1,m+1) - f (i+2,m) := by
 
-/-
-lemma pattern_nContinuant2 (F : Type*) [Field F] (f : ℕ×ℕ → F) (n: ℕ) [nzPattern_n F f n] : ∀ i, i ≤ n+1 → ∀m, f (n-i,m) = f (n,m)*f (n-i+1,m-1) - f (n-i+2,m-2) := by
- intro i
+
+suffices pattern_nContinuant2flipped : ∀ i, i ≤ n+1 → ∀m, f (n-i-1,m+2) = f (n-1,m+2)*f (n-i,m+1) - f (n-i+1,m)
+·sorry
+
+·intro i h
  induction i with
  | zero =>
- intro _ m
+ intro m
  simp
- have hₙ : f (n+1,m-1) = 1 ∧ f (n+2,m-2) = 0 := by exact ⟨pattern_n.botBordOnes_n (m-1), nzPattern_n.botBordZeroes (m -2)⟩
+ have hₙ : f (n,m+1) = 1 ∧ f (n+1,m) = 0 := by exact ⟨pattern_n.botBordOnes_n (m+1), pattern_n.botBordZeros_n (n+1) m (by linarith)⟩
  rw[hₙ.2,hₙ.1,sub_zero,mul_one]
  | succ k ih =>
- intro h m
- have a₁ : m = m - 1 + 1 := by sorry
- have a₂ : n - k + 1 = n - (k+1) + 2 := by sorry
- --Have a bunch of issues here due to this being defined on ℕ instead of ℤ, ask Antoine if there's a fix
- have a₃ : n-k+1 ≤ n+1 := by sorry
- have a₄ : n-k = n - (k+1)+1 := by sorry
- have a₅ : k ≤ n+1 := by sorry --have k≤k+1≤ n+1, how to apply transitivity to this?
- have a₆ : m-2+1 = m-1 := by sorry
+ intro m
+ have a₁ : n - (k + 1) - 1 + 2 = n - k := by sorry
+ have a₂ : n - (k + 1) - 1 + 1 = n - k - 1 := by sorry
+ have a₃ : 1 ≤ n - (k + 1) - 1 + 2 ∧ n - (k + 1) - 1 + 2 ≤ n := by sorry
+ have a₅ : k ≤ n+1 := by sorry --have k≤k+1≤ n+1, apply transitivity to this
+ have a₇ : n - (k + 1) - 1 ≤ n-1 := by sorry
+ have a₉ : n - (k + 1) - 1 + 1 + 2 = n - k + 1 := by sorry
+ have a₁₀ : n - (k + 1) - 1 + 1 ≤ n-1 := by sorry
+ have a₁₁ : n - (k + 1) - 1 + 1 = n -(k+1) := by sorry
+ have a₁₂ : n - (k + 1) - 1 + 2 = n - k := by sorry
+ have a₁₃ : n - (k + 1) - 1 + 2 = n - (k + 1) + 1 := by sorry
+
+ have h₂ : f (n - (k + 1) - 1 + 2, m + 1) ≠ 0 := by exact nzPattern_n.non_zero (n - (k + 1) - 1 + 2) (m + 1) a₃
+
+ calc f (n - (k + 1) - 1, m + 2) =  f (n - (k + 1) - 1, m+2) * f (n - (k + 1) - 1 + 2, m + 1) * (f (n - (k + 1) - 1 + 2, m + 1))⁻¹ := by rw [mul_inv_cancel_right₀ h₂ (f (n - (k + 1) - 1, m+2))]
+  _= f (n - (k + 1) - 1 + 2, m + 1) * f (n - (k + 1) - 1, (m + 1) + 1) * (f (n - (k + 1) - 1 + 2, m + 1))⁻¹ := by ring
+  _= ( f (n - (k + 1) - 1 + 1, m + 1) * f (n - (k + 1) - 1 + 1, (m + 1) + 1) - 1) * (f (n - (k + 1) - 1 + 2, m + 1))⁻¹ := by rw[pattern_n.diamond (n - (k + 1) - 1) (m+1) a₇]
+  _= ( f (n - (k + 1) - 1 + 1, m + 1) * f (n - (k + 1) - 1 + 1, m + 2) - 1) * (f (n - (k + 1) - 1 + 2, m + 1))⁻¹ := by simp
+  _= ( f (n - (k + 1) - 1 + 1, m + 1) * f (n - k - 1, m + 2) - 1) * (f (n - (k + 1) - 1 + 2, m + 1))⁻¹ := by rw[a₂]
+  _= ( f (n - (k + 1) - 1 + 1, m + 1) * (f (n - 1, m + 2) * f (n - k, m + 1) - f (n - k + 1, m)) - 1) * (f (n - (k + 1) - 1 + 2, m + 1))⁻¹ := by rw[← ih a₅]
+  _= (f (n - 1, m + 2) * f (n - (k + 1) - 1 + 1, m + 1) * f (n - k, m + 1) - f (n - k + 1, m) * f (n - (k + 1) - 1 + 1, m + 1) - 1) * (f (n - (k + 1) - 1 + 2 , m + 1))⁻¹ := by ring
+  _= (f (n - 1, m + 2) * f (n - (k + 1) - 1 + 1, m + 1) * f (n - k, m + 1) - f (n - (k + 1) - 1 + 1 + 2, m) * f (n - (k + 1) - 1 + 1, m + 1) - 1) * (f (n - (k + 1) - 1 + 2 , m + 1))⁻¹ := by rw[a₉]
+  _= (f (n - 1, m + 2) * f (n - (k + 1) - 1 + 1, m + 1) * f (n - k, m + 1) - (f (n - (k + 1) - 1 + 1+1,m) * f (n - (k + 1) - 1 + 1 + 1, m + 1) - 1) - 1) * (f (n - (k + 1) - 1 + 2 , m + 1))⁻¹ := by rw[pattern_n.diamond (n - (k + 1) - 1 + 1) (m) a₁₀]
+  _= f (n - 1, m + 2) * f (n - (k + 1) - 1 + 1, m + 1) * f (n - k, m + 1) * (f (n - (k + 1) - 1 + 2, m + 1))⁻¹ - f (n - (k + 1) - 1 + 2,m) * f (n - (k + 1) - 1 + 2 , m + 1) * (f (n - (k + 1) - 1 + 2 , m + 1))⁻¹  := by ring
+  _= f (n - 1, m + 2) * f (n - (k + 1), m + 1) * f (n - (k + 1) - 1 + 2, m + 1) * (f (n - (k + 1) - 1 + 2, m + 1))⁻¹ - f (n - (k + 1) - 1 + 2, m) * f (n - (k + 1) - 1 + 2 , m + 1) * (f (n - (k + 1) - 1 + 2 , m + 1))⁻¹  := by rw[a₁₁, a₁₂]
+  _= f (n - 1, m + 2) * f (n - (k + 1), m + 1) - f (n - (k + 1) - 1 + 2, m) := by rw[mul_inv_cancel_right₀ h₂ (f (n - 1, m + 2) * f (n - (k + 1), m + 1)), mul_inv_cancel_right₀ h₂ (f (n - (k + 1) - 1 + 2, m))]
+  _= f (n - 1, m + 2) * f (n - (k + 1), m + 1) - f (n - (k + 1) + 1, m) := by rw[a₁₃]
+
+  -- used a₂ a₃ a₅ a₇ a₅ a₉ a₁₀ a₁₁ a₁₂ a₁₃
+
+  --_= f (n - (k + 1) + 2 , m - 1) * f (n - (k + 1), m-1+1) * (f (n - k + 1, m - 1))⁻¹ := by rw[a₂]
+  --_= (f (n - (k+1)+1,m-1) * f (n - (k+1)+1, m-1+1) - 1) * (f (n - k + 1, m - 1))⁻¹ := by rw[pattern_n.diamond n (m-1) (n-(k+1))]
+  --_= (f (n - k,m-1) * f (n - k, m) - 1) * (f (n - k + 1, m - 1))⁻¹ := by rw[← a₁, ← a₄]
+  --_= (f (n - k,m-1) * (f (n, m) * f (n - k + 1, m - 1) - f (n - k + 2, m - 2)) - 1) * (f (n - k + 1, m - 1))⁻¹ := by rw[← ih a₅]
+  --_= (f (n, m)*f (n - k,m-1)*f (n - k + 1, m - 1) - f (n - k + 2, m - 2)*f (n - k,m-1) - 1) * (f (n - k + 1, m - 1))⁻¹ := by ring
+  --_= (f (n, m)*f (n - k,m-1)*f (n - k + 1, m - 1) - f (n - k + 2, m - 2)*f (n - k,(m-2)+1) - 1) * (f (n - k + 1, m - 1))⁻¹ := by rw[a₆]
+  --_= (f (n, m)*f (n - k,m-1)*f (n - k + 1, m - 1) - (f (n-k+1,m-2) * f (n-k+1,m-1)-1) - 1) * (f (n - k + 1, m - 1))⁻¹ := by rw[← a₆,pattern_n.diamond n (m-2) (n-k)]
+  --_= f (n, m)*f (n - k,m-1)*f (n - k + 1, m - 1)* (f (n - k + 1, m - 1))⁻¹  - f (n-k+1,m-2) * f (n-k+1,m-1)* (f (n - k + 1, m - 1))⁻¹  := by ring
+  --_= f (n, m)*f (n - k,m-1) - f (n-k+1,m-2) := by rw [mul_inv_cancel_right₀ h₂ (f (n, m)*f (n - k,m-1)), mul_inv_cancel_right₀ h₂ (f (n-k+1,m-2))]
 
 
- have h₂ : f (n - k + 1, m - 1) ≠ 0 := by exact nzPattern_n.non_zero (n-k+1) (m - 1) a₃
-
- calc f (n - (k + 1), m) = f (n - (k + 1), m) * f (n - k + 1, m - 1) * (f (n - k + 1, m - 1))⁻¹ := by rw [mul_inv_cancel_right₀ h₂ (f (n - (k + 1), m))]
-  _= f (n - k + 1, m - 1) * f (n - (k + 1), m) * (f (n - k + 1, m - 1))⁻¹ := by rw[mul_comm (f (n - k + 1, m - 1))]
-  _= f (n - (k + 1) + 2 , m - 1) * f (n - (k + 1), m-1+1) * (f (n - k + 1, m - 1))⁻¹ := by rw[a₂,← a₁]
-  _= (f (n - (k+1)+1,m-1) * f (n - (k+1)+1, m-1+1) - 1) * (f (n - k + 1, m - 1))⁻¹ := by rw[pattern_n.diamond n (m-1) (n-(k+1))]
-  _= (f (n - k,m-1) * f (n - k, m) - 1) * (f (n - k + 1, m - 1))⁻¹ := by rw[← a₁, ← a₄]
-  _= (f (n - k,m-1) * (f (n, m) * f (n - k + 1, m - 1) - f (n - k + 2, m - 2)) - 1) * (f (n - k + 1, m - 1))⁻¹ := by rw[← ih a₅]
-  _= (f (n, m)*f (n - k,m-1)*f (n - k + 1, m - 1) - f (n - k + 2, m - 2)*f (n - k,m-1) - 1) * (f (n - k + 1, m - 1))⁻¹ := by ring
-  _= (f (n, m)*f (n - k,m-1)*f (n - k + 1, m - 1) - f (n - k + 2, m - 2)*f (n - k,(m-2)+1) - 1) * (f (n - k + 1, m - 1))⁻¹ := by rw[a₆]
-  _= (f (n, m)*f (n - k,m-1)*f (n - k + 1, m - 1) - (f (n-k+1,m-2) * f (n-k+1,m-1)-1) - 1) * (f (n - k + 1, m - 1))⁻¹ := by rw[← a₆,pattern_n.diamond n (m-2) (n-k)]
-  _= f (n, m)*f (n - k,m-1)*f (n - k + 1, m - 1)* (f (n - k + 1, m - 1))⁻¹  - f (n-k+1,m-2) * f (n-k+1,m-1)* (f (n - k + 1, m - 1))⁻¹  := by ring
-  _= f (n, m)*f (n - k,m-1) - f (n-k+1,m-2) := by rw [mul_inv_cancel_right₀ h₂ (f (n, m)*f (n - k,m-1)), mul_inv_cancel_right₀ h₂ (f (n-k+1,m-2))]
 
 
-
--/
 
 theorem trsltInv (F : Type*) [Field F] (f : ℕ×ℕ → F) (n: ℕ) [nzPattern_n F f n] : ∀ i, i ≤ n+1 → ∀m, f (i,m) = f (i,m+n+1) := by
   -- it suffices to prove glide symmetry
