b4efa9f73e5a5f31acfecd9468eb1e451cf6d196
JonCheah <jon.mh.cheah@gmail.com>
2024-08-21 20:18:35 +0800

    Update chapter3.lean

--- FriezePatterns/chapter3.lean
+++ FriezePatterns/chapter3.lean
@@ -72,7 +72,52 @@ def friezeToFlute1 (f : ℕ×ℕ → ℚ) (n m: ℕ) (hn : 2 ≤ n) [arith_fp f 
     unfold flute_f
     simp [n_sub_one_div_i]                      --finish if i%(n-1) ≠ 0
 
-  have div : ∀ i, flute_f f n m (i + 1) ∣ (flute_f f n m (i) + flute_f f n m (i + 2)) := by sorry
+  have div : ∀ i, flute_f f n m (i + 1) ∣ (flute_f f n m (i) + flute_f f n m (i + 2)) := by
+    intro i
+
+    by_cases n_eq_two : n=2
+    unfold flute_f
+    simp [n_eq_two]
+    simp [Nat.mod_one]
+
+    by_cases n_eq_three : n=3
+
+    by_cases i_even : i%2 = 0
+    have i_plus_one_odd : (i+1)%2 = 1 := by omega
+    have i_plus_two_even : (i+2)%2 = 0 := by rw[Nat.add_mod_right i 2, i_even]
+
+    unfold flute_f
+    simp [n_eq_three]
+    simp [i_even, i_plus_one_odd, i_plus_two_even]
+    rw[@pattern_n.topBordOnes ℚ _ f n _ m]
+    simp                                          --finish if n=2, i_even
+
+    have i_odd : i%2 = 1 := by omega
+    have i_plus_one_even : (i+1)%2 = 0 := by omega
+    have i_plus_two_odd : (i+2)%2 = 1 := by omega
+    unfold flute_f
+    simp [n_eq_three]
+    simp [i_plus_one_even]                        --finish if n=2, i_odd
+
+    have n_gt_three : n>3 := by omega
+
+    by_cases n_sub_one_div_i : i%(n-1) = 0
+    have zero_lt_n_sub_one : 0 < n - 1 := by omega
+    have h : (n-1) ∣ i := by omega
+    have i_plus_one_mod_n_sub_one : ¬( (n-1) ∣ (i+1) ) := by sorry
+    have i_plus_two_mod_n_sub_one : ¬( (n-1) ∣ (i+2) ) := by sorry
+    have h1 : (i+1)%(n-1) ≠ 0 := by omega
+    have h2 : (i+2)%(n-1) ≠ 0 := by omega
+    unfold flute_f
+    simp[n_sub_one_div_i, h1, h2]
+    have a₁ : (i)%(n-1) ≤ n-1 := by sorry
+    have a₂ : (i+2)%(n-1) = (i)%(n-1) + 2 := by sorry
+    rw[a₂]
+    rw[pattern_nContinuant1 ℚ f n ((i)%(n-1)) a₁ m]
+    sorry
+
+    sorry
+
 
   exact ⟨flute_f f n m, pos, hd, period, div⟩
 
